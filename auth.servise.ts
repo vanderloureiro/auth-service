133ff08031dce69d034e5e5493afaad09c521cad
Vanderlei Loureiro <vandeloureiro@gmail.com>
2019-07-05 10:41:07 -0300

    Upload

---
+++ auth.servise.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators'; 
+
+const API_URL = 'http://localhost:3000';
+
+@Injectable({
+    providedIn: 'root
+})
+export class AuthService{
+
+    constructor(private http: HttpClient){}
+
+    autenticate(userName: string, password: string){
+	return this.http
+	    .post(
+		API_URL + '/user/login',
+		{ userName, password },
+		{ observe: 'response' }
+	    )
+	    .pipe(tap(res => {
+		const authToken = res.headers.get('x-access-token');
+		console.log(authToken);
+	    }))
+    }
+
+}
+
+
