71d14f3ece165265a60f8e46cce8a4e89453f0a7
Vanderlei Loureiro <vandeloureiro@gmail.com>
2019-07-05 11:08:19 -0300

    Update auth.servise.ts

--- auth.servise.ts
+++ auth.servise.ts
@@ -1,29 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators'; 
+import { tap } from 'rxjs/operators';
+import { TokenService } from './token.service';
 
 const API_URL = 'http://localhost:3000';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class AuthService{
+export class AuthService {
 
-    constructor(private http: HttpClient){}
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
-    autenticate(userName: string, password: string){
-	return this.http
-	    .post(
-		API_URL + '/user/login',
-		{ userName, password },
-		{ observe: 'response' }
-	    )
-	    .pipe(tap(res => {
-		const authToken = res.headers.get('x-access-token');
-		console.log(authToken);
-	    }))
-    }
+  authenticate(userName: string, password: string) {
 
+    return this.http
+      .post(
+        API_URL + '/user/login', 
+        { userName, password }, 
+        { observe: 'response'} 
+      )
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
+        console.log(`User ${userName} authenticated with token ${authToken}`);
+      }));
+  }
 }
-
-
